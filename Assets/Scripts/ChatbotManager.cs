413be3580012e3e5ac4d0e1f86086b2404580583
karol <kslowinski1@edu.cdv.pl>
2018-02-03 18:28:09 +0100

    initial commit

---
+++ Assets/Scripts/ChatbotManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ChatbotManager: MonoBehaviour
+{
+    private Chatbot bot;
+    public InputField inputField;
+    public Text gentlemanOutput;
+
+    void Start()
+    {
+        bot = new Chatbot();
+        bot.LoadBrain();
+    }
+
+    void OnDisable()
+    {
+        bot.SaveBrain();
+    }
+
+    public void SendQuestionToRobot()
+    {
+        if (string.IsNullOrEmpty(inputField.text) == false)
+        {
+            var answer = bot.getOutput(inputField.text);
+            gentlemanOutput.text = answer;
+            inputField.text = string.Empty;
+        }
+    }
+
+
+}
